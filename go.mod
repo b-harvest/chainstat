51917cabd24eceb9d6c679ff39cf1e8dc78a5b81
zsystm <actor93kor@gmail.com>
2024-10-16 14:29:14 +0900

    feat: average txs, block time, tps

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/b-harvest/chainstat
+
+go 1.23.1
+
+require github.com/cometbft/cometbft v0.38.12 // indirect
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
+)
+
+func main() {
+	// connect to a running node
+	rpcAddress := "http://localhost:26657"
+	client, err := rpchttp.New(rpcAddress, "/websocket")
+	if err != nil {
+		log.Fatalf("failed to create RPC client: %v", err)
+	}
+
+	args := os.Args[1:]
+	// validate the number of arguments
+	if len(args) != 2 {
+		fmt.Println("Usage: go run main.go <start_block_height> <end_block_height>")
+		os.Exit(1)
+	}
+	startBlockHeight := args[0]
+	endBlockHeight := args[1]
+	if startBlockHeight > endBlockHeight {
+		fmt.Println("start_block_height must be less than end_block_height")
+		os.Exit(1)
+	}
+	startBlockNum, err := strconv.ParseInt(startBlockHeight, 10, 64)
+	if err != nil {
+		fmt.Println("start_block_height must be a valid integer")
+		os.Exit(1)
+	}
+	endBlockNum, err := strconv.ParseInt(endBlockHeight, 10, 64)
+	if err != nil {
+		fmt.Println("end_block_height must be a valid integer")
+		os.Exit(1)
+	}
+
+	// Arrays to store the statistics for each 100-block range
+	var avgTxsPer100Blocks []float64
+	var avgBlockTimePer100Blocks []float64
+	var avgTPSPer100Blocks []float64
+
+	// Loop through blocks in 100-block intervals
+	for i := startBlockNum; i <= endBlockNum; i += 100 {
+		var totalTxs int
+
+		// Define the last block in the current 100-block range
+		endRange := i + 99
+		if endRange > endBlockNum {
+			endRange = endBlockNum
+		}
+
+		// Process each block in the 100-block range
+		for j := i; j <= endRange; j++ {
+			// Get the current block result
+			currBlockResult, err := client.BlockResults(context.Background(), &j)
+			if err != nil {
+				log.Fatalf("failed to get block results: %v", err)
+			}
+			totalTxs += len(currBlockResult.TxsResults)
+		}
+
+		// Get block time difference between the start and end block
+		startBlockInfo, err := client.Block(context.Background(), &i)
+		if err != nil {
+			log.Fatalf("failed to get block: %v", err)
+		}
+		endBlockInfo, err := client.Block(context.Background(), &endRange)
+		if err != nil {
+			log.Fatalf("failed to get block: %v", err)
+		}
+		totalTimeDiff := endBlockInfo.Block.Header.Time.Sub(startBlockInfo.Block.Header.Time).Seconds()
+
+		// Calculate the averages for this 100-block range
+		avgTxs := float64(totalTxs) / float64(endRange-i+1)
+		avgBlockTime := totalTimeDiff / float64(endRange-i) // n-1 for block time differences
+		tps := float64(totalTxs) / totalTimeDiff
+
+		// Store the values in arrays for later use
+		avgTxsPer100Blocks = append(avgTxsPer100Blocks, avgTxs)
+		avgBlockTimePer100Blocks = append(avgBlockTimePer100Blocks, avgBlockTime)
+		avgTPSPer100Blocks = append(avgTPSPer100Blocks, tps)
+
+		// Output the statistics for this range
+		fmt.Printf("Blocks %d to %d:\n", i, endRange)
+		fmt.Printf("  Average Tx Count: %.2f\n", avgTxs)
+		fmt.Printf("  Average Block Time: %.2f seconds\n", avgBlockTime)
+		fmt.Printf("  Average TPS: %.2f\n\n", tps)
+	}
+
+	// You can use avgTxsPer100Blocks, avgBlockTimePer100Blocks, avgTPSPer100Blocks for plotting graphs later
+}
